b03a7f2a3a9afb647f1e9ac9b912aee381ad80ff
MuShaf-NMS <nurmuhammadshafa@gmail.com>
2022-09-27 14:49:57 +0800

    add some basics request

--- app/index.js
+++ app/index.js
@@ -1,5 +1,25 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express(); // init app
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    name: "express-js",
+    version: "1.0.0",
+    description: "express js App",
+    repository: "github.com/MuShaf-NMS/todo-app-express-js.git",
+    author: "MuShaf-NMS",
+    license: "MIT",
+    private: false,
+  });
+});
+
+app.post("/", (req, res) => {
+  return res.json({
+    test: req.body.test,
+  });
+});
 
 export default app;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import app from "./app/index.js";
 
+const port = 8000;
+
 // run server on specified port
-app.listen(8000, () => {
-  console.log("halo");
+app.listen(port, () => {
+  console.log(`Development server is running on:\n * http://localhost:${port}`);
 });
